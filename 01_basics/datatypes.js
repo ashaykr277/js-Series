16dd66f183bdf5484cc8a26261527a3a7f5beab4
Ashay Kumar <141417857+ashaykr277@users.noreply.github.com>
2024-12-16 17:32:00 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,11 @@
+//  number => 2 to power 52
+//  bigInt 
+//  string => " "
+//  boolean => true/false
+//  null => standalone value
+// undefined =>
+// symbol => unique
+
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);
