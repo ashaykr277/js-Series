126e3f528a4855f6c368806b9a70b7f0965131e3
Ashay Kumar <141417857+ashaykr277@users.noreply.github.com>
2024-12-17 09:38:53 +0000

    series

--- 01_basics/variables.js
+++
@@ -1,8 +0,0 @@
-const accId=12345;
-let accEmail="ashaykr277@gmail.com";
-var accPass="841863";
-accCity="Banglore"
-
-//accId=321654;     cannot be changed
-console.log(accId)
-console.table([accId,accEmail, accPass,accCity])
