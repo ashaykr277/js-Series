98ccc994ea503abbc70b7776f830fa341ae40fce
Ashay Kumar <141417857+ashaykr277@users.noreply.github.com>
2024-12-17 09:38:33 +0000

    commit changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accId=12345;
+let accEmail="ashaykr277@gmail.com";
+var accPass="841863";
+accCity="Banglore"
+
+//accId=321654;     cannot be changed
+console.log(accId)
+console.table([accId,accEmail, accPass,accCity])
---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,18 @@
+//  number to string
+let score=100;
+console.log(score);
+console.log(typeof (score));
+let valueString=String(score);
+
+console.log(typeof (valueString));
+console.log(valueString);
+
+console.log("---------------");
+//  string to number
+
+let num="10"
+console.log(typeof (num));
+console.log(num);
+let stringNum=Number(num);
+console.log(typeof (stringNum));
+console.log(stringNum);
