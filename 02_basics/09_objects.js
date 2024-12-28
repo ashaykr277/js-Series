4cea5a15323eb0725205810a9871a9b2ba9cccc2
Ashay Kumar <141417857+ashaykr277@users.noreply.github.com>
2024-12-28 08:05:20 +0000

    obj

--- 02_basics/09_objects.js
+++ 02_basics/09_objects.js
@@ -27,12 +27,70 @@ const user={
 // user.email="ashay@chatgpt.com"      //as above is freeze therefore no changes is allowed to be done
 // console.log(user["email"]);
 //----------------------------------------------------
-user.greeting=function(){
-    console.log("hello user")
-}
-console.log(user.greeting());
+// user.greeting=function(){
+//     console.log("hello user")
+// }
+// console.log(user.greeting());
 
-user.greetings=function(){
-    console.log(`hello user ${this.name}`)
+// user.greetings=function(){
+//     console.log(`hello user ${this.name}`)
+// }
+// console.log(user.greetings());
+
+console.log("------------part 2-----------");
+
+const user2={};
+user2.name="abcd";
+user2.email="abcd@gmail.com";
+// console.log(user2)
+
+//----------------------
+
+const JsUser={
+    email:"ashaykr@gmail.com",
+    fullname:{
+        username:{
+            firstname:"Ashay",
+            lastname:"Kumar",
+        }
+    }
 }
-console.log(user.greetings());
+console.log(JsUser.fullname)
+console.log(JsUser.fullname.username)
+console.log(JsUser.fullname.username.firstname)
+
+// how to join multiple objects in to a single object
+const obj1={1:"a",2:"b"};
+const obj2={3:"c",4:"d"};
+const obj3=Object.assign({},obj1, obj2)     //extra {} at start means all data will come to this {}, is not added then all will come to obj1
+console.log(obj3)
+// or the best way is 
+const obj4={...obj1, ...obj2}
+console.log(obj4);
+
+//if there is array of objects in database then how to access
+const dbuser=[
+    {
+        name:"adam",
+        email:"123@gmail.com"
+    },
+    {
+        name:"bob",
+        email:"456@gmail.com"
+    },
+    {
+        name:"ces",
+        email:"789@gmail.com"
+    },
+]
+console.log(dbuser[1].email);
+
+// to print all keys, values of an object
+console.log(Object.keys(user))
+console.log(Object.values(user))
+console.log(Object.entries(user))       //converts pair of key value in arrays
+
+// to check an object has its own property or not?
+console.log(user.hasOwnProperty("isLoggedIn"))
+console.log(user.hasOwnProperty("isLoggedOut"))
+ 
