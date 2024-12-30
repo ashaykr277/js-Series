1d3b00b9eced9814226ef9118c46a721a117719a
Ashay Kumar <141417857+ashaykr277@users.noreply.github.com>
2024-12-30 17:43:10 +0000

    this

--- 03_basics/04_arrowFnctn.js
+++ 03_basics/04_arrowFnctn.js
@@ -8,3 +8,26 @@ const user={
 user.welcomeMessage();
 user.username="sam";        //this will update form current username
 user.welcomeMessage();
+//------------------------------------
+console.log("----------------")
+function hlo(){
+    let username="Ashay"
+    console.log(this.username);     //this will show undefined becouse, "this" is only used in object but this is function so it wont work, hence undefined
+}
+hlo();
+
+// Arrow function
+const addTwo=(a,b)=>{
+    return a+b;
+}
+console.log(addTwo(2,3));
+
+//it can also be written in single line
+const sum2=(a,b)=> a+ b;
+
+console.log(sum2(2,3));
+
+// it can return objects too
+const returnObj=()=>({username:"ashay"});
+
+console.log(returnObj());
